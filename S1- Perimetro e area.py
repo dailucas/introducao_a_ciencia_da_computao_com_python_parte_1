4290d3160cc6cb6838ebbd3d76a9b36532e48cf9
Lucas Yamamoto <102343025+dailucas@users.noreply.github.com>
2022-04-17 21:23:22 -0300

    Delete S1- Perimetro e area.py

--- S1- Perimetro e area.py
+++
@@ -1,9 +0,0 @@
-def quadrado(l):
-    perimetro =  l * 4
-    area = l ** 2
-    print(f'perímetro: {perimetro} - área: {area}')
-    return perimetro, area
-
-l = int(input('Digite o valor correspondente ao lado de um quadrado: '))
-
-quadrado(l)
